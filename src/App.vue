80a1c053c8eae706c32ff195766e7a52d9e9f034
Ngân <tuyetnganphamhoang@gmail.com>
2019-06-04 19:57:42 +0700

    override old style

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,18 @@
 </template>
 
 <style>
+  *, html {
+    margin: 0;
+    padding: 0;
+  }
+  .black, .white, .blue {
+    background-color: inherit !important;
+  }
+  .cg-board-wrap {
+    width: 500px !important;
+    height: 500px !important;
+  }
+  .cg-board-wrap coords {
+    font-size: 20px !important;
+  }
 </style>
